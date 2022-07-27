7809a3fd75383a55a635f15ac192a8afd7afe4c1
alexjr17 <alexjose.r.r@gmail.com>
2022-07-26 20:37:16 -0500

    cargar los templates en el archivo views.py

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'catalogos\index.html')
+
+def create(request):
+    return render(request, 'catalogos\create.html')
+
+def edit(request):
+    return render(request, 'catalogo.edit.html')
