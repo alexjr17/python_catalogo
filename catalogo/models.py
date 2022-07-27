801606fdc3551805648f9c5bab5c5d95e8d3a340
alexjr17 <alexjose.r.r@gmail.com>
2022-07-27 12:14:42 -0500

    se creo el modelo de producto

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,16 @@
+from ctypes import sizeof
+from distutils.command.upload import upload
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+class Producto(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(verbose_name="Name", max_length=200)
+    size = models.CharField(verbose_name="Size", max_length=200)
+    observations = models.TextField(verbose_name="Observations", null=True)
+    brand = models.CharField(verbose_name="Brand", max_length=200)
+    inventory = models.IntegerField(verbose_name="Inventory", default=0)
+    image = models.ImageField(verbose_name="Image", upload_to='images/', null=True)
+    date = models.DateTimeField(verbose_name="Date")
