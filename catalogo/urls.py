31a3b43a53cd147846bce850dcba04f32c8eed9a
alexjr17 <alexjose.r.r@gmail.com>
2022-07-28 15:47:30 -0500

    imagen se muestra y se instalo tailwind al proyecto

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.index, name="catalogos_index"),
     path('create', views.create, name="catalogos_create"),
---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['../catalogo/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
