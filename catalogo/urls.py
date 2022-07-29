93a2a96bcc75cfd63eae2286b8ebae8ec7495d3f
alexjr17 <alexjose.r.r@gmail.com>
2022-07-29 12:49:49 -0500

    crear form producto

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="catalogos_index"),
     path('create', views.create, name="catalogos_create"),
-    path('edit', views.edit, name="catalogos_edit")
+    path('edit', views.edit, name="catalogos_edit"),
+    path('store', views.store, name="catalogos_store")
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from .models import Producto
+from .forms import ProductoForm
 # Create your views here.
 
 def index(request): 
+    items_table = ['id', 'Nombre', 'talla', 'observaciones' , 'marca', 'inventario', 'Imagen', 'Fecha', 'funciones']
     productos = Producto.objects.all()
-    print(productos)
-    return render(request, 'catalogos\index.html', {'productos': productos})
+    return render(request, 'catalogos\index.html', {'productos': productos, 'items': items_table})
 
 def create(request):
-    return render(request, 'catalogos\create.html')
+    form_producto = ProductoForm
+    return render(request, 'catalogos\create.html', {'form_producto': form_producto})
 
 def edit(request):
     return render(request, 'catalogos\edit.html')
+
+def store(request):
+    print('datos')
+    return  request
